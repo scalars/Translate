f9c14ac28947959bfa441d46ba1851632515ff96
David Ceron <davidaceronc@gmail.com>
2021-06-22 19:18:42 -0500

    Index page with ApplicationList implemented

---
+++ components/ApplicationCard.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-card
+        hover
+        width="300px"
+        style="margin: auto; display: flex; justify-content: center; align-items: center; flex-flow: column;"
+    >
+        <v-card-title class="text-h5" @click="selectApplication">
+            {{ application.name }}
+        </v-card-title>
+
+        <div style="display: flex">
+            <v-chip
+                v-for="(lang, index) in application.languages"
+                :key="index"
+                :class="{ 'mx-1': true, 'primary': lang.isolanguage === application.defaultLanguage.isolanguage }"
+            >
+                {{ lang.isolanguage }}
+            </v-chip>
+        </div>
+
+        <v-card-actions>
+            <GeneralButton
+                icon-mode
+                icon="mdi-eye"
+                :text="'Show Application'"
+                @click="selectApplication"
+            />
+            <GeneralButton
+                icon-mode
+                icon="mdi-delete"
+                :text="'Delete Application'"
+                @click="deleteApplication"
+            />
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator';
+import { Application } from '@/client/types';
+import GeneralButton from '@/components/general/GeneralButton.vue';
+
+@Component( {
+    components: { GeneralButton }
+} )
+
+export default class ApplicationCard extends Vue {
+    @Prop( { default () { return {}; } } ) application: Application;
+
+    selectApplication () {
+        this.$emit( 'selectApplication' );
+    }
+
+    deleteApplication () {
+        this.$emit( 'deleteApplication' );
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/ApplicationsList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="applications-list-container">
+        <ApplicationCard
+            v-for="application in applications"
+            :key="application.id"
+            :application="application"
+            @selectApplication="selectApplication"
+            @deleteApplication="selectApplication"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+import ApplicationCard from '@/components/ApplicationCard.vue';
+import { Application } from '@/client/types';
+
+@Component( {
+    components: { ApplicationCard }
+} )
+export default class ApplicationsList extends Vue {
+    @Prop( { default () { return []; } } ) applications: Application[];
+
+    selectApplication ( application: Application ) {
+        this.$store.commit( 'sessionStorage/application', application );
+        this.$router.push( { name: 'translations' } );
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.applications-list-container {
+    padding: 30px 0;
+    display: grid;
+    grid-template-columns: auto auto auto;
+    gap: 15px;
+    @include mobile {
+        grid-template-columns: auto;
+    }
+}
+</style>
