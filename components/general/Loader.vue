bc3ddd1b12bef3731ad4567a3bce1aa9131644b8
David Ceron <davidaceronc@gmail.com>
2021-06-29 10:51:31 -0500

    Loader component added to be reused

---
+++ components/general/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-sheet class="loading primary--text" :height="height" :width="width">
+        <v-progress-circular
+            indeterminate
+            rounded
+        />
+    </v-sheet>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator';
+@Component
+export default class Loading extends Vue {
+    @Prop( { type: String, default: '100%' } ) height: string;
+    @Prop( { type: String, default: '100%' } ) width: string;
+}
+</script>
+<style lang="scss" scoped>
+.loading {
+    padding: 5%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
