d31309bf0f3a36dfa30a1571bccfb797c92cae41
David Ceron <davidaceronc@gmail.com>
2021-06-29 13:47:41 -0500

    LanguagesForm and Languages page implemented

---
+++ components/languages/LanguagesForm.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="languages-form-container">
+        <Loading v-if="loading" />
+        <v-form v-else class="languages-form">
+            <v-select
+                v-model="selectedLanguages"
+                :items="languages"
+                return-object
+                multiple
+                chips
+                deletable-chips
+                :item-text="getLanguageLabel"
+                label="Languages"
+            >
+                <!--                TODO Evaluate if this alternative looks better -->
+                <!--                <template #item="{item}">-->
+                <!--                    <v-avatar size="22" class="mx-1">-->
+                <!--                        <v-img-->
+                <!--                            :src="item.flag"-->
+                <!--                        />-->
+                <!--                    </v-avatar>-->
+                <!--                    <div class="ml-2 mr-2">-->
+                <!--                        {{ getLanguageLabel( item ) }}-->
+                <!--                    </div>-->
+                <!--                </template>-->
+            </v-select>
+        </v-form>
+        <GeneralButton
+            :width="$vuetify.breakpoint.smAndDown ? '' : '200px'"
+            :loading="loading"
+            text="Save"
+            @click="saveLanguages"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'nuxt-property-decorator';
+import Loading from '@/components/general/Loader.vue';
+import GeneralButton from '@/components/general/GeneralButton.vue';
+import { Language } from '@/client/types';
+
+@Component( {
+    components: { Loading, GeneralButton }
+} )
+export default class LanguagesForm extends Vue {
+    @Prop( { required: true } ) languages: Language[];
+    @Prop( {} ) addedLanguages: Language[];
+    @Prop( { default: false } ) loading: boolean;
+    selectedLanguages: Language[] = [];
+
+    @Watch( 'addedLanguages' )
+    onAddedLanguagesChanged () {
+        this.selectedLanguages = this.addedLanguages || [];
+    }
+
+    beforeMount () {
+        this.selectedLanguages = this.addedLanguages || [];
+    }
+
+    getLanguageLabel ( language: Language ) {
+        return `${language.isolanguage} | ${language.namelanguage} | ${language.nativename}`;
+    }
+
+    saveLanguages () {
+        this.$emit( 'saveLanguages', this.selectedLanguages );
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.languages-form-container {
+    display: flex;
+    flex-flow: column;
+    align-items: flex-end;
+    padding: 15px;
+    .languages-form {
+        width: 100%;
+        margin-bottom: 15px;
+    }
+}
+</style>
--- pages/languages.vue
+++ pages/languages.vue
@@ -1,15 +1,154 @@
 <template>
-    <div>Languages</div>
+    <div>
+        <Modal
+            v-model="showAddLanguage"
+            :title="'Updating Languages'"
+        >
+            <LanguagesForm
+                :languages="allLanguages"
+                :added-languages="addedLanguages"
+                :loading="loading"
+                @saveLanguages="updateLanguages"
+            />
+        </Modal>
+        <LanguagesTable
+            :languages="addedLanguages"
+            :loading="loading"
+        >
+            <template #general-options>
+                <GeneralButton
+                    icon="mdi-plus"
+                    text="Add Language"
+                    @click="addLanguage"
+                />
+            </template>
+            <template #item-options="{ item }">
+                <GeneralButton
+                    icon-mode
+                    icon="mdi-delete"
+                    text="Delete Language"
+                    @click="deleteLanguage(item)"
+                />
+            </template>
+        </LanguagesTable>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
+import LanguagesTable from '@/components/languages/LanguagesTable.vue';
+import LanguagesForm from '@/components/languages/LanguagesForm.vue';
+import Modal from '@/components/general/Modal.vue';
+import GeneralButton from '@/components/general/GeneralButton.vue';
+import { Language } from '@/client/types';
 
 @Component( {
-    components: {},
+    components: { LanguagesTable, LanguagesForm, GeneralButton, Modal },
     layout: 'application'
 } )
-export default class Languages extends Vue {
+export default class LanguagesPage extends Vue {
+    allLanguages: Language[] = [];
+    addedLanguages: Language[] = [];
+    loading: boolean = false;
+    showAddLanguage: boolean = false;
+
+    get application () {
+        return this.$store.getters['sessionStorage/getApplication'];
+    }
+
+    beforeMount () {
+        this.getLanguages();
+    }
+
+    async getLanguages () {
+        try {
+            this.loading = true;
+            const { languages } = await this.$apiClient.queries.languages( {} );
+            this.allLanguages = languages;
+            this.addedLanguages = languages.filter( ( language: Language ) =>
+                this.application.languages.some( ( lang: Language ) =>
+                    lang.id === language.id ) );
+        } catch ( error ) {
+            console.error( error );
+        } finally {
+            this.loading = false;
+        }
+    }
+
+    async updateLanguages ( languages: Language[] ) {
+        const hasChanged = this.validateIfLanguagesChanged( languages );
+        if ( hasChanged ) {
+            try {
+                const languagesToAdd: Language[] = languages.filter( ( language: Language ) =>
+                    !this.addedLanguages.some( ( lang: Language ) => lang.id === language.id ) );
+                const languagesToRemove: Language[] = this.addedLanguages.filter( ( language: Language ) =>
+                    !languages.some( ( lang: Language ) => lang.id === language.id ) );
+                this.loading = true;
+                // TODO Uncomment code when the mutation returns the data correctly
+                // const { updateApplication } =
+                await this.$apiClient.queries.updateApplication( {
+                    where: { id: this.application.id },
+                    data: {
+                        languages: {
+                            connect: languagesToAdd.map( ( language: Language ) => { return { id: language.id }; } ),
+                            disconnect: languagesToRemove.map( ( language: Language ) => { return { id: language.id }; } )
+                        }
+                    }
+                } );
+                const updateApplication = { ...this.application, languages: [...languages] };
+                this.addedLanguages = [...updateApplication.languages];
+                this.$store.commit( 'sessionStorage/setApplication', updateApplication );
+                this.showAddLanguage = false;
+            } catch ( error ) {
+                console.error( error );
+            } finally {
+                this.loading = false;
+            }
+        }
+    }
+
+    addLanguage () {
+        console.log( 'adding' );
+        this.showAddLanguage = true;
+    }
+
+    async deleteLanguage ( language: Language ) {
+        try {
+            this.loading = true;
+            // TODO Uncomment code when the mutation returns the data correctly
+            // const { updateApplication } =
+            await this.$apiClient.queries.updateApplication( {
+                where: { id: this.application.id },
+                data: {
+                    languages: {
+                        disconnect: [{ id: language.id }]
+                    }
+                }
+            } );
+            const indexToRemove = this.addedLanguages.findIndex( ( lang: Language ) => language.id === lang.id );
+            this.addedLanguages.splice( indexToRemove, 1 );
+            const updateApplication = { ...this.application, languages: [...this.addedLanguages] };
+            this.addedLanguages = [...updateApplication.languages];
+            this.$store.commit( 'sessionStorage/setApplication', updateApplication );
+        } catch ( error ) {
+            console.error( error );
+        } finally {
+            this.loading = false;
+        }
+    }
+
+    validateIfLanguagesChanged ( languages: Language[] ) {
+        if ( !languages ) {
+            return true;
+        } else if ( this.addedLanguages.length !== languages.length ) {
+            return true;
+        } else {
+            return !( this.addedLanguages.every( ( language: Language ) =>
+                languages.some( ( lang: Language ) => lang.id === language.id ) ) &&
+                languages.every( ( language: Language ) =>
+                    this.addedLanguages.some( ( lang: Language ) => lang.id === language.id ) ) );
+        }
+    }
 }
 </script>
 
